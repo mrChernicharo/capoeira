31eb4bd5eb20309d443c10f973e96fbe8bc91b60
Felipe Chernicharo <string7dev@gmail.com>
2021-07-10 22:57:53 -0400

    messing around

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
-import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import React, { Suspense } from "react";
+import { Canvas, Vector3 } from "@react-three/fiber";
+import { OrbitControls, Box, Environment } from "@react-three/drei";
 import Abe from "./CapoeiraAbe";
 
 import "./index.css";
 
 export default function App() {
+  const vec: Vector3 = [1, 0.1, 2];
+
   return (
-    <Canvas>
-      <OrbitControls />
-      <directionalLight intensity={2.5} />
-      <ambientLight intensity={1.2} />
-      <Abe />
-    </Canvas>
+    <>
+      <h1>Hello Capoeira</h1>
+
+      <Canvas>
+        <OrbitControls />
+        <directionalLight intensity={2.5} />
+        <ambientLight intensity={1.2} />
+        <Suspense fallback={"...Loading"}>
+          <Abe />
+          <Box scale={0.2} position={vec} />
+        </Suspense>
+      </Canvas>
+    </>
   );
 }
